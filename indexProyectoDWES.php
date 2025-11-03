28df48d3584104dc58cf0d6299a483042217d551
EnriqueNieto90 <enrikeniet90@gmail.com>
2025-11-03 09:32:53 +0100

    Initial commit: Carga inicial del proyecto

---
+++ indexProyectoDWES.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>CFGS - Desarrollo de Aplicaciones Web</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: #f4f6f9;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background: green;
+            color: white;
+            padding: 15px;
+            text-align: center;
+        }
+        h1 {
+            margin: 0;
+        }
+        main {
+            max-width: 800px;
+            margin: 30px auto;
+            padding: 20px;
+            background: white;
+            border-radius: 10px;
+            box-shadow: 0 4px 10px rgba(0,0,0,0.1);
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+        }
+        li {
+            background: #ecf0f1;
+            margin: 10px 0;
+            padding: 15px;
+            border-left: 5px solid green;
+            border-right: 5px solid green;
+            transition: 0.3s;
+	    border-radius:8px;
+        }
+        li:hover {
+            background: #d6eaf8;
+            border-left: 5px solid purple;
+            border-right: 5px solid purple;
+        }
+
+        footer{
+            margin: auto;
+            background-color: green;
+            text-align: center;
+            height: 150px;
+	    color: white;
+        }
+	main{
+	text-align:center;
+	justify-content:center;
+	}
+
+    </style>
+</head>
+<body>
+    <header>
+        <h2>CFGS - Desarrollo de Aplicaciones Web</h2>
+        <h1><b>Enrique Nieto Lorenzo</b></h1>
+        <p>Curso 2025/2026 - Grupo DAW2 - IES Los Sauces</p>
+    </header>
+    <main>
+        <h2><b>TEMARIO DESARROLLO DE APLICACIONES EN ENTORNO SERVIDOR</b></h2>
+        <ul>
+            <li><a href="doc/Tema1Ejercicios.pdf" target="_blank">TEMA 1 - DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+            <li><a href="doc/README.html" target="_blank">TEMA 2 - INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>
+            <li><a href="/ENLDWESProyectoTema3/indexProyectoTema3.php">TEMA 3 - CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>
+            <li><a href="/ENLDWESProyectoTema4/indexProyectoTema4.php">TEMA 4 - TÉCNICAS DE ACCESO A DATOS EN PHP</li>
+            <li>TEMA 5 - DESARROLLO DE APLICACIONES WEB</li>
+            <li>TEMA 6 - APLICACIONES WEB MULTICAPA</li>
+            <li>TEMA 7 - PROGRAMACIÓN DE SERVICIOS WEB</li>
+            <li>TEMA 8 - DESARROLLO DE APLICACIONES WEB HÍBRIDAS</li>
+        </ul>
+    </main>
+
+    <footer>
+        <caption>
+            <a href="/">Enrique Nieto Lorenzo</a> | 03/10/2025
+        </caption>
+    </footer>
+</body>
+</html>
