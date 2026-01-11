2457a5b21f99c4a087fa70b5ff6f68d323ed7a4e
Enrique <Enrique@DESKTOP-9BJR8IC>
2026-01-11 20:49:17 +0100

    fix[backend]: modificaciones en estilos

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Enrique Nieto | DWES</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
+    <link rel="stylesheet" href="webroot/css/fonts.css">
+    <link rel="stylesheet" href="webroot/css/all.min.css">
     <link rel="stylesheet" href="webroot/css/estilosdwes.css">
 </head>
 <body>
@@ -62,7 +60,7 @@
                     <ul class="lista-ejercicios">
                         <li>
                             <a href="https://github.com/EnriqueNieto90/ENLDAWProyectoDAW" target="_blank">
-                                <i class="fa-brands fa-github"></i> Estudio Tema 2
+                                <i class="fa-brands fa-github"></i> Documentación Tema 2
                             </a>
                         </li>
                         <li>
